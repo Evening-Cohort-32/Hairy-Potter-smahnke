99f4248a9d3397ecdccaac8148d234830f101aba
Stephanie Mahnke <steph.mahnke@gmail.com>
2026-03-14 00:02:24 +0100

    Added pottery objects

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,22 @@
 // Imports go first
-
+import {makePottery} from './PotteryWheel.js';
 
 
 // Make 5 pieces of pottery at the wheel
-let mug = makePottery()
+let mug = makePottery("round", 3, 3)
+console.log(mug)
+
+let plate = makePottery("square", 5, 1)
+console.log(plate)
+
+let vase = makePottery("cylinder", 4, 8)
+console.log(vase)
+
+let bowl = makePottery("round", 4, 4)
+console.log(bowl)
+
+let planter = makePottery("rectangle", 6, 9)
+console.log(planter)
 
 
 // Fire each piece of pottery in the kiln
